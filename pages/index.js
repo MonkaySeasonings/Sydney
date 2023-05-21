24982cb1eb12c88ca375362bf7c15b7f3cafa7f0
MonkaySeasonings <112832868+MonkaySeasonings@users.noreply.github.com>
2023-05-21 10:42:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>My Bloggings</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="My Bloggings" />
         <hr />
         <p className="description">
           There's a lot to see and do on the web and when I'm note experimenting with scripts, I'm trying to <a href="https://www.freetubetv.net">watch free tv online</a> to find wacky international shows.
